41cd745d8c39b6b93e925c6a0b631859a2df2f66
jhonasn <jhonasn@gmail.com>
2018-03-08 01:23:42 -0300

    using comon variable to set default duration time

--- src/shared/notify.js
+++ src/shared/notify.js
@@ -22,16 +22,16 @@ export default {
   info (msg, icon = 'info', time = notifyDefaultTime) {
     _notify('info', msg, icon, time)
   },
-  success (msg, icon = 'check', time = 3000) {
+  success (msg, icon = 'check', time = notifyDefaultTime) {
     _notify('success', msg, icon, time)
   },
-  warning (msg, icon = 'warning', time = 3000) {
+  warning (msg, icon = 'warning', time = notifyDefaultTime) {
     _notify('warning', msg, icon, time)
   },
-  danger (msg, icon = 'error', time = 3000) {
+  danger (msg, icon = 'error', time = notifyDefaultTime) {
     _notify('danger', msg, icon, time)
   },
-  other (msg, icon = 'notifications', time = 3000) {
+  other (msg, icon = 'notifications', time = notifyDefaultTime) {
     _notify('', msg, icon, time)
   }
 }
