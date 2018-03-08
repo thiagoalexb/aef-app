c112d155e1ffb936ddf288f669d6ff7abe23fac0
jhonasn <jhonasn@gmail.com>
2018-03-07 21:05:00 -0300

    implemented method to handle common api responses

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,3 +1,5 @@
+import notify from './notify'
+
 export default {
   convertDateInObject (obj) {
     for (let key in obj) {
@@ -21,5 +23,20 @@ export default {
   },
   randomAefColors () {
     return ['red', 'orange', 'blue'][this.randomBetween(0, 2)]
+  },
+  handleApiError (data, attemptedAction = 'executar uma requisição ao servidor') {
+    let { message, messages, error, errors } = data
+
+    if (messages || errors) {
+      if (errors && errors.length) messages = errors
+      message = '<ul>'
+      messages.forEach(m => {
+        message += `<li>${m}</li>`
+      })
+      message = '</ul>'
+    } else if (error) message = error
+    else if (!message) message = `Ocorreu um problema ao ${attemptedAction}`
+
+    notify.danger(message)
   }
 }
