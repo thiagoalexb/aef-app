573f6715891d0e3a1bca3b335f2f6b0af61f6276
jhonasn <jhonasn@gmail.com>
2018-03-08 19:31:49 -0300

    moved login component to shared components

--- src/components/shared/Login.vue
+++ src/components/shared/Login.vue
@@ -19,7 +19,7 @@
 
             <div class="row">
               <div class="col-md-12">
-                <div class="form-group label-floating">
+                <div class="form-group label-floating form-info">
                   <label class="control-label">E-mail</label>
                   <input
                     type="email"
@@ -33,7 +33,7 @@
             </div>
             <div class="row">
               <div class="col-md-12">
-                <div class="form-group label-floating">
+                <div class="form-group label-floating form-info">
                   <label class="control-label">Senha</label>
                   <input
                     type="password"
