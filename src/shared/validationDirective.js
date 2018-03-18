17ecda5ea4c0e24eeba51d10c0aaa0bc196f03ce
jhonasn <jhonasn@gmail.com>
2018-03-18 16:04:22 -0300

    implemented validation directive to use more than one in one component

--- src/shared/validationDirective.js
+++ src/shared/validationDirective.js
@@ -38,9 +38,9 @@ function getValidationObject (el, binding) {
   }
 }
 
-function validate (el, binding) {
+function validate (el, binding, anotherValidationDirectives) {
   const validation = getValidationObject(el, binding)
-  const iconContainer = el.querySelector('.form-control-feedback')
+  const iconContainer = el.querySelector(`.form-control-feedback.${validation.icon}`)
   const messageContainer = el.querySelector('.help-block')
 
   if (binding.value) {
@@ -49,11 +49,23 @@ function validate (el, binding) {
     }
     iconContainer.style.display = 'block'
     messageContainer.style.display = 'block'
-    messageContainer.innerHTML = binding.value
+
+    // option that passes boolean for directive
+    const msg = typeof binding.value === 'string'
+      ? binding.value
+      : ''
+
+    messageContainer.innerHTML = msg
   } else {
     el.classList.remove(validation.class)
+
     iconContainer.style.display = 'none'
-    messageContainer.style.display = 'none'
+    // only hide messages if another directives has no value to show
+    if (!anotherValidationDirectives ||
+      !anotherValidationDirectives.length ||
+      !anotherValidationDirectives.some(d => !!d.value)) {
+      messageContainer.style.display = 'none'
+    }
   }
 }
 
@@ -61,21 +73,37 @@ export default {
   bind (el, binding) {
     const validation = getValidationObject(el, binding)
     const input = el.querySelector('input, textarea, select')
+
+    // option for another validation directive on component
+    const messageContainer = el.querySelector('.help-block')
+
     const template = document.createElement('template')
+    // use it own icon
     template.innerHTML = `
-    <span class="form-control-feedback" style="display: none">
-      <i class="material-icons">${validation.icon}</i>
-    </span>
-    <span class="help-block" style="display: none">${binding.value || ''}</span>
+      <span class="form-control-feedback ${validation.icon}" style="display: none">
+        <i class="material-icons">${validation.icon}</i>
+      </span>
     `
+    if (!messageContainer) {
+      const msg = typeof binding.value === 'string' && binding.value
+        ? binding.value
+        : ''
+
+      template.innerHTML += `
+        <span class="help-block" style="display: none">${msg}</span>
+      `
+      input.after(template.content.children[0])
+    }
 
     // when we append after input it removes the node from template content
-    input.after(template.content.children[0])
     input.after(template.content.children[0])
 
     validate(el, binding)
   },
-  update (el, binding) {
-    validate(el, binding)
+  update (el, binding, vnode) {
+    const anotherValidationDirectives = vnode.data.directives.filter(d =>
+      d.name === 'validation' &&
+      d !== binding)
+    validate(el, binding, anotherValidationDirectives)
   }
 }
