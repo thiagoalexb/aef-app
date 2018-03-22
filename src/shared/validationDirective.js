dc431dcf378d36ed1a9da71dfcce025c9f7c8cf6
jhonasn <jhonasn@gmail.com>
2018-03-22 00:50:56 -0300

    changed hide/show icons logic to preserve not validation component feedback icons

--- src/shared/validationDirective.js
+++ src/shared/validationDirective.js
@@ -41,12 +41,15 @@ function getValidationObject (el, binding) {
 function validate (el, binding, anotherValidationDirectives) {
   const validation = getValidationObject(el, binding)
   const iconContainer = el.querySelector(`.form-control-feedback.${validation.icon}`)
+  const otherIcons = el.querySelectorAll('.form-control-feedback')
+  const notValidationIcons = el.querySelectorAll('.form-control-feedback:not(.validation)')
   const messageContainer = el.querySelector('.help-block')
 
   if (binding.value) {
     if (el.className.indexOf(validation.class) === -1) {
       el.classList.add(validation.class)
     }
+    if (otherIcons.length) otherIcons.forEach(i => { i.style.display = 'none' })
     iconContainer.style.display = 'block'
     messageContainer.style.display = 'block'
 
@@ -60,6 +63,9 @@ function validate (el, binding, anotherValidationDirectives) {
     el.classList.remove(validation.class)
 
     iconContainer.style.display = 'none'
+
+    // show not validation icons
+    if (notValidationIcons.length) notValidationIcons.forEach(i => { i.style.removeProperty('display') })
     // only hide messages if another directives has no value to show
     if (!anotherValidationDirectives ||
       !anotherValidationDirectives.length ||
@@ -80,7 +86,7 @@ export default {
     const template = document.createElement('template')
     // use it own icon
     template.innerHTML = `
-      <span class="form-control-feedback ${validation.icon}" style="display: none">
+      <span class="form-control-feedback validation ${validation.icon}" style="display: none">
         <i class="material-icons">${validation.icon}</i>
       </span>
     `
