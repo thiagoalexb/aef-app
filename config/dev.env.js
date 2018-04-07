44b5a29d035013dea8cc30c68b4cf167a862f13c
jhonasn <jhonasn@gmail.com>
2018-04-07 02:03:20 -0300

    changed api url mantaining remote url copy

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"https://aefapi.azurewebsites.net/api/"'
+  // API_URL: '"https://aefapi.azurewebsites.net/api/"'
+  API_URL: '"http://localhost:5000/api/"'
 })
